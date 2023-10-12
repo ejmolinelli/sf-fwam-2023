9fcb317359f27a470f4d10fee2742cb360fb4b4f
Evan Molinelli <molinelli.evan@gmail.com>
2023-10-12 11:04:11 -0400

    added renderloop aid

--- hellopoint.js
+++ hellopoint.js
@@ -3,13 +3,13 @@
 var VSHADER_SOURCE = 
   'void main() {\n' +
   '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Set the vertex coordinates of the point
-  '  gl_PointSize = 10.0;\n' +                    // Set the point size
+  '  gl_PointSize = 60.0;\n' +                    // Set the point size
   '}\n';
 
 // Fragment shader program
 var FSHADER_SOURCE =
   'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // Set the point color
+  '  gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);\n' + // Set the point color
   '}\n';
 
 function main() {
